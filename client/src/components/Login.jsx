4d2c55c55ceb561764d25b20eda143c75ecc001d
kay <kaych26@gmail.com>
2020-05-16 15:12:50 -0400

    css login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import Hero from './Hero';
+import '../styles/Login.css';
 
 export default class Login extends Component {
   state = {
@@ -18,50 +19,60 @@ export default class Login extends Component {
   };
 
   render() {
- 
     const { username, email, password } = this.state;
     return (
       <>
-      <Hero />
-      <Link to="/register">click here to Register</Link>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          this.props.handleLogin(this.state);
-          this.props.history.push('/');
-        }}
-      >
-        <h3 className="form-key">Login</h3>
-        <label htmlFor="username">username:</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          value={username}
-          onChange={this.handleChange}
-        />
-        <br />
-        {/* <label htmlFor="email">email:</label>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-        /> */}
-        <br />
-        <label htmlFor="password">password:</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-        />
-        <br />
-        <button>Submit</button>
-        </form>
-        </>
+        <Hero />
+
+        <div className="login-outerframe">
+          <Link to="/register" className="register-link">
+            Click here to Register
+          </Link>
+
+          <h3 className="login-form-title">Login</h3>
+
+          <form
+            className="login-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              this.props.handleLogin(this.state);
+              this.props.history.push('/');
+            }}
+          >
+            <div className="login-div">
+              <label className="username" htmlFor="username">
+                username: {' '}
+              </label>
+              <input
+                className="login-input"
+                id="username"
+                type="text"
+                name="username"
+                value={username}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="login-div">
+              <label className="password" htmlFor="password">
+                password:{' '}
+              </label>
+              <input
+                className="login-input"
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="login-button-frame">
+              <button className="login-button">Submit</button>
+            </div>
+          </form>
+        </div>
+      </>
     );
   }
 }
