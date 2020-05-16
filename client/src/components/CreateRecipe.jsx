1ec5697230aedac027394f5482983be0079bd663
kay <kaych26@gmail.com>
2020-05-16 19:44:40 -0400

    css for create form

--- client/src/components/CreateRecipe.jsx
+++ client/src/components/CreateRecipe.jsx
@@ -32,20 +32,22 @@ export default class CreateRecipe extends Component {
     });
   };
   render() {
-    
     return (
       <div className="createrecipe-outerframe">
         {this.props.currentUser && this.assignUserId}
         <h3 className="createrecipe-title">Create Recipe </h3>
-        <select onChange={this.handleSelect}>
-          <option>Select a Category</option>
-          {this.props.categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.group}
-            </option>
-          ))}
-        </select>
+        <div className="create-select-frame">
+          <select onChange={this.handleSelect} className="create-select">
+            <option>Select a Category</option>
+            {this.props.categories.map((category) => (
+              <option key={category.id} value={category.id} >
+                {category.group}
+              </option>
+            ))}
+          </select>
+        </div>
         <form
+          className="create-form-frame"
           id="create_form"
           onSubmit={(e) => {
             e.preventDefault();
@@ -54,42 +56,66 @@ export default class CreateRecipe extends Component {
             this.props.history.push('/');
           }}
         >
+          <label className="create-form-label" htmlFor="image">
+            image url:{' '}
+          </label>
           <input
+            className="update-input"
             id="update_image"
             type="text"
             name="image"
             value={this.state.image}
             onChange={this.handleChange}
           />
+          <label className="create-form-label" htmlFor="name">
+            recipe name:{' '}
+          </label>
           <input
+            className="update-input"
             id="update_name"
             type="text"
             name="name"
             value={this.state.name}
             onChange={this.handleChange}
           />
+          <label className="create-form-label" htmlFor="prep_time">
+            prep time:{' '}
+          </label>
           <input
+            className="update-input"
             id="update_prep_time"
             type="text"
             name="prep_time"
             value={this.state.prep_time}
             onChange={this.handleChange}
           />
+          <label className="create-form-label" htmlFor="ingredient">
+            ingredient:{' '}
+          </label>
           <input
+            className="update-input"
             id="update_ingredient"
             type="text"
             name="ingredient"
             value={this.state.ingredient}
             onChange={this.handleChange}
           />
+          <label className="create-form-label" htmlFor="direction">
+            direction{' '}
+          </label>
           <input
+            className="update-input"
             id="update_direction"
             type="text"
             name="direction"
             value={this.state.direction}
             onChange={this.handleChange}
           />
+          <label className="create-form-label" htmlFor="story">
+            tell your story:{' '}
+          </label>
           <input
+            className="update-input"
             id="update_story"
             type="text"
             name="story"
