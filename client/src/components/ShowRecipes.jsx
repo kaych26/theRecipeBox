524ede4d8005416286e5c796b640be74058d2981
kay <kaych26@gmail.com>
2020-05-18 00:53:00 -0400

    updated button css

--- client/src/components/ShowRecipes.jsx
+++ client/src/components/ShowRecipes.jsx
@@ -5,21 +5,22 @@ import '../styles/ShowRecipes.css';
 export default function ShowRecipes(props) {
   return (
     <div className="showrecipes-outerframe">
-        <h1 className="showrecipes-cat-name">{props.category} Recipes</h1>
-      <div className="showrecipes-title-frame">
+      {/* <div className="showrecipes-title-frame"> */}
 
-        {props.currentUser && (
-          <div className="showrecipe-button">
-            <Link to="/recipe/create">
-              <button>Create</button>
-            </Link>
-          </div>
-        )}
-      </div>
+      {props.currentUser && (
+       
+        <div className="showrecipe-add-frame">
+          <Link to="/recipe/create">
+            {/* Share your recipes and stories! */}
+            <button className="showrecipe-add-button">Share your recipes & stories here!</button>
+          </Link>
+        </div>
+      )}
+      <h1 className="showrecipes-cat-name">{props.category} Recipes</h1>
+      {/* </div> */}
 
       <div className="showrecipes-wrapframe">
-      {props.recipes ?
-        (
+        {props.recipes ? (
           props.recipes.map((recipe) => (
             <React.Fragment key={recipe.id}>
               <Link to={`/recipes/${recipe.id}`}>
@@ -43,8 +44,9 @@ export default function ShowRecipes(props) {
               </Link>
             </React.Fragment>
           ))
-        
-        ): <h2>Need recipes ...</h2> }
+        ) : (
+          <h2>Need recipes ...</h2>
+        )}
       </div>
     </div>
   );
